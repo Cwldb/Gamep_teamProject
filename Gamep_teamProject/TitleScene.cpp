fd3d8f072128a259ba0111ebfc1a627d8b8f409c
Parksungje <Parksungje0213@gmail.com>
2025-06-09 16:01:32 +0900

    merge

--- Gamep_teamProject/TitleScene.cpp
+++ Gamep_teamProject/TitleScene.cpp
@@ -29,45 +29,45 @@ void TitleSceneUpdate(Scene& _eCurScene)
 	}
 }
 
-Menu GetCurMenu()
-{
-	COORD resolution = GetConsoleResolution();
-	int x = resolution.X / 3;
-	static int y = resolution.Y / 3 * 2;
-	static int originy = y;
-	Key eKey = KeyController();
-	// 2 
-	switch (eKey)
-	{
-	case Key::UP:
-		if (y > originy)
-		{
-			Gotoxy(x - 2, y);
-			cout << " ";
-			Gotoxy(x - 2, --y);
-			cout << ">";
-			Sleep(100);
-		}
-		break;
-	case Key::DOWN:
-		if (y < originy + 2)
-		{
-			Gotoxy(x - 2, y);
-			cout << " ";
-			Gotoxy(x - 2, ++y);
-			cout << ">";
-			Sleep(100);
-			// 1 
-		}
-		break;
-	case Key::SPACE:
-		if (originy == y) return Menu::START;
-		else if (originy + 1 == y) return Menu::INFO;
-		else  if (originy + 2 == y) return Menu::QUIT;
-		break;
-	}
-	return Menu::FAIL;
-}
+//Menu GetCurMenu()
+//{
+//	COORD resolution = GetConsoleResolution();
+//	int x = resolution.X / 3;
+//	static int y = resolution.Y / 3 * 2;
+//	static int originy = y;
+//	Key eKey = KeyController();
+//	// 2 
+//	switch (eKey)
+//	{
+//	case Key::UP:
+//		if (y > originy)
+//		{
+//			Gotoxy(x - 2, y);
+//			cout << " ";
+//			Gotoxy(x - 2, --y);
+//			cout << ">";
+//			Sleep(100);
+//		}
+//		break;
+//	case Key::DOWN:
+//		if (y < originy + 2)
+//		{
+//			Gotoxy(x - 2, y);
+//			cout << " ";
+//			Gotoxy(x - 2, ++y);
+//			cout << ">";
+//			Sleep(100);
+//			// 1 
+//		}
+//		break;
+//	case Key::SPACE:
+//		if (originy == y) return Menu::START;
+//		else if (originy + 1 == y) return Menu::INFO;
+//		else  if (originy + 2 == y) return Menu::QUIT;
+//		break;
+//	}
+//	return Menu::FAIL;
+//}
 
 void TitleSceneRender()
 {
