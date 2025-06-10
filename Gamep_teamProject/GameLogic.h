338b0a5cd62296f03c52d24f9dc978a2b3b2bdb5
Cwldb <mozi0810@gmail.com>
2025-06-10 11:35:35 +0900

    add playerStruct

--- Gamep_teamProject/GameLogic.h
+++ Gamep_teamProject/GameLogic.h
@@ -4,6 +4,34 @@
 const int MAP_HEIGHT = 20;
 const int MAP_WIDTH = 21;
 
+typedef struct _pos
+{
+    int x;
+    int y;
+    bool operator == (_pos other)
+    //bool operator== (const _pos& other) const
+    {
+        return (x == other.x && y == other.y);
+    }
+
+}POS, * PPOS;
+
+typedef struct _playerstate
+{
+    int coinCnt; // 코인 개수
+}PLAYERSTATE, * PPLAYERSTATE;
+
+typedef struct _playerpos
+{
+    POS tPos;
+    POS tNewPos;
+}PLAYERPOS, * PPLAYERPOS;
+
+typedef struct _player
+{
+    PLAYERPOS position;
+    PLAYERSTATE state;
+}PLAYER, * PPLAYER;
 
 void Init();
 
