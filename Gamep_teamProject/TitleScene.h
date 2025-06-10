fe8f45f989b403f33535f113be93fd3f3f719031
Parksungje <Parksungje0213@gmail.com>
2025-06-10 11:13:25 +0900

    feat : SceneChange

--- Gamep_teamProject/TitleScene.h
+++ Gamep_teamProject/TitleScene.h
@@ -5,3 +5,4 @@
 void TitleScene(Scene& _eCurScene);
 void TitleSceneUpdate(Scene& _eCurScene);
 void TitleSceneRender();
+Menu GetCurMenu();
--- Gamep_teamProject/main.cpp
+++ Gamep_teamProject/main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include "TitleScene.h"
+#include "GameLogic.h"
 
 int main()
 {
-	
+	Scene curScene = Scene::TITLE;
+	while (curScene != Scene::QUIT)
+	{
+		switch (curScene)
+		{
+		case Scene::TITLE:
+			TitleScene(curScene);
+			break;
+		case Scene::GAME:
+			GameScene(curScene);
+		}
+	}
 }
