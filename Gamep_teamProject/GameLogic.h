8718ea42de0840527131192d5e0a2a91c9a12144
Parksungje <Parksungje0213@gmail.com>
2025-06-16 17:14:59 +0900

    feat : DDONG Scrolling

--- Gamep_teamProject/GameLogic.h
+++ Gamep_teamProject/GameLogic.h
@@ -6,6 +6,7 @@ using std::vector;
 const int MAP_HEIGHT = 24;
 const int MAP_WIDTH = 22;
 
+
 typedef struct _pos
 {
     int x;
@@ -50,9 +51,9 @@ void Update(char _gameMap[MAP_HEIGHT][MAP_WIDTH], PPLAYER _pPlayer, vector<DDONG
 void Render(char _gameMap[MAP_HEIGHT][MAP_WIDTH], PPLAYER _pPlayer);
 void LoadStage(char _gameMap[MAP_HEIGHT][MAP_WIDTH]);
 void GameScene(Scene& _eCurScene, char _gameMap[MAP_HEIGHT][MAP_WIDTH], PPLAYER _pPlayer, vector<DDONG> vecDDONG);
+void SpawnDDong(char _gameMap[MAP_HEIGHT][MAP_WIDTH], vector<DDONG>& vecDDONG);
 void InfoScene(Scene& _eCurScene);
 void RenderInfo();
 
-void SpawnDDong(char _gameMap[MAP_HEIGHT][MAP_WIDTH], vector<DDONG> vecDDONG);
 void PlayerInit(char _gameMap[MAP_HEIGHT][MAP_WIDTH], PPLAYER _pPlayer);
 void HandleInput(char _gameMap[MAP_HEIGHT][MAP_WIDTH], PPLAYER _pPlayer);
